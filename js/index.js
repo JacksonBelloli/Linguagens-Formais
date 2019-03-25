f56fba7f28d6d895fcb8d88ba4dacfd3cd63ddb1
JacksonBelloli <jakson.belloli@gmail.com>
2019-03-25 16:47:27 -0300

    algumas alterações

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
-$(document).ready(function() {
-    var cond = true;
-    var nt = ['S'];
-    var exp = [];
-    var first = '';
-    var i = 0;
+function CatchGram (exp){
+    var nt = [];
 
-    while (cond) {
-        for (var index = 0; index <= nt.length-1; index++) {
-            //console.log(nt[index]);
-            first = prompt('Digite ' + nt[index] + ':');
-            exp = first.split('');
-        }
-        for (var index = 0; index <= exp.length-1; index++) {
-            if ((index == 0) || (index == 1)) {
-                continue;
-            }
-            if ((exp[index].charCodeAt(0) >= 65) && (exp[index].charCodeAt(0) <= 90)){                
-                nt [i] = exp[index];
+    nt = exp.split(':');
 
-                i++;
-            }else if (index == exp.length){
-                console.log('passou aqui!');
-                cond = false;
-                break;
-            }
-        }   
+    return nt;
+};
+function CatchExps (exp){
+
+    var res = [];
+
+    res = exp.split('|');
+
+    return res;
+};
+function CreateStack (exp){
+
+    var str = [];
+    var stack = [];
+
+    str = exp.split('');
+
+    for (var i = str.length-1; i >= 0; i--){
+        stack.push(str[i]);
     }
-}); 
+    return stack;
+};
+function randomize (QuantidadeExp){
+    return Math.floor((Math.random() * QuantidadeExp) + 1);
+};
---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    const gram = 'S:aSb|ab';
+    
+    var nt = [];    
+
+    nt = CatchGram(gram);
+
+    var data = new Array([nt[0]]);
+
+    nt = CatchExps(nt[1]);
+    for (var i = 0; i <= nt.length-1; i++){
+        data [0].push(nt[i]);
+    }
+    var st = [];
+
+    st = CreateStack(data[0][randomize(nt.length)]);
+    for (var i = st.length-1; i >= 0; i--){
+        
+    }
+
+    /* var cond = true;
+    var nt = ['S'];
+    var exp = [];
+    var first = '';
+    var i = 0;
+
+    while (cond) {
+        for (var index = 0; index <= nt.length-1; index++) {
+            //console.log(nt[index]);
+            first = prompt('Digite ' + nt[index] + ':');
+            exp = first.split('');
+        }
+        for (var index = 0; index <= exp.length-1; index++) {
+            if ((index == 0) || (index == 1)) {
+                continue;
+            }
+            if ((exp[index].charCodeAt(0) >= 65) && (exp[index].charCodeAt(0) <= 90)){                
+                nt [i] = exp[index];
+
+                i++;
+            }else if (index == exp.length){
+                console.log('passou aqui!');
+                cond = false;
+                break;
+            }
+        }   
+    }*/
+}); 
